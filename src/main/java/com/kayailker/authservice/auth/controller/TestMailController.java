a962dc300f7afffa41275c6794e5b824647b5739
kayailkerugur <kayailkerugur@gmail.com>
2025-04-27 00:47:15 +0300

    chore(config): ignore application.properties and provide application-example.properties

--- src/main/java/com/kayailker/authservice/auth/controller/TestMailController.java
+++ src/main/java/com/kayailker/authservice/auth/controller/TestMailController.java
@@ -1,2 +1,36 @@
-package com.kayailker.authservice.auth.controller;public class TestMailController {
+package com.kayailker.authservice.auth.controller;
+
+import com.kayailker.authservice.auth.service.MailService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test-mail")
+public class TestMailController {
+    private final MailService mailService;
+
+    public TestMailController(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> sendTestEmail(@RequestParam String to) {
+        String body = """
+    Merhaba Ortekay,
+
+    Bu, sistemimiz tarafından gönderilen test amaçlı bir e-postadır.
+
+    Eğer bu e-postayı beklemiyorsanız lütfen dikkate almayınız.
+
+    İyi günler dileriz!
+    
+    - RestLocation Auth Team
+    """;
+        mailService.sendEmail(to, "Test Mail", body);
+
+        return ResponseEntity.ok("Mail gönderildi!");
+    }
 }
--- src/main/java/com/kayailker/authservice/auth/service/MailService.java
+++ src/main/java/com/kayailker/authservice/auth/service/MailService.java
@@ -1,2 +1,26 @@
-package com.kayailker.authservice.auth.service;public class MailService {
+package com.kayailker.authservice.auth.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    public MailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("seninmailadresin@gmail.com"); // Buraya kendi email adresini yaz
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+    }
+
 }
